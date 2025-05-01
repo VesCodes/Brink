0c8a923868a4d40a2163ac5af0d200a84cac76b5
VesCodes <VesCodes@users.noreply.github.com>
2025-05-01 19:59:05 +0100

    Add VSCode config files
    Generate compilation database via `Build.bat -MJ .vscode/compile_commands.json`

--- Build.sh
+++ Build.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 
+mkdir -p Build
+
 emcc -std=c++20 -g -Wall -Wextra -Werror -Wsign-conversion -Wno-unused-parameter -Wno-missing-designated-field-initializers -ferror-limit=0 \
     Source/Brink.cpp Source/BrinkSandbox.cpp --shell-file Source/Brink.html -o Build/index.html -sUSE_WEBGPU=1 $@
