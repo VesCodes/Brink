0b3b3ecc2d68e3db92708aeb54fd26e990bf9adf
VesCodes <VesCodes@users.noreply.github.com>
2025-05-02 15:25:25 +0100

    Print error message on assert/fatal error

--- Source/BkCore/BkCore.cpp
+++ Source/BkCore/BkCore.cpp
@@ -1,18 +1,56 @@
 #include "BkCore.h"
+#include "BkString.h"
 
+#include <stdio.h>
 #include <stdlib.h>
 
 namespace Bk
 {
 	bool AssertError(const char* expr, const char* file, int32 line, const char* format, ...)
 	{
-		// #TODO: Error message
+		TStringBuffer<1024> errorMessage;
+		errorMessage.Appendf("ASSERTION FAILED: %s [%s:%d]\n", expr, file, line);
+
+		if (format && format[0] != '\0')
+		{
+			va_list args;
+			va_start(args, format);
+
+			if (errorMessage.Appendv(format, args))
+			{
+				errorMessage.Append('\n');
+			}
+
+			va_end(args);
+		}
+
+		fwrite(errorMessage.data, sizeof(char), errorMessage.length, stderr);
+		fflush(stderr);
+
 		return true;
 	}
 
 	void FatalError(int32 exitCode, const char* format, ...)
 	{
-		// #TODO: Error message
+		TStringBuffer<1024> errorMessage;
+		errorMessage.Appendf("FATAL ERROR: %d\n", exitCode);
+
+		if (format && format[0] != '\0')
+		{
+			va_list args;
+			va_start(args, format);
+
+			if (errorMessage.Appendv(format, args))
+			{
+				errorMessage.Append('\n');
+			}
+
+			va_end(args);
+		}
+
+		fwrite(errorMessage.data, sizeof(char), errorMessage.length, stderr);
+		fflush(stderr);
+
 		exit(exitCode);
 	}
 }
