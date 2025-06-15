829a00d4bf742ad7dc6b1b0e3c0504c5513e4f9a
VesCodes <VesCodes@users.noreply.github.com>
2025-06-15 22:46:40 +0100

    Rename sandbox source files

--- Build.bat
+++ Build.bat
@@ -4,5 +4,5 @@ if not exist Build mkdir Build
 
 emcc -std=c++20 -g -gsource-map -Wall -Wextra -Werror -ferror-limit=0 ^
     -Wsign-conversion -Wno-unused-parameter -Wno-missing-designated-field-initializers ^
-    Source/BrinkSandbox.cpp --shell-file Source/Brink.html -o Build/index.html ^
+    Source/BkSandbox.cpp --shell-file Source/BkSandbox.html -o Build/index.html ^
     --use-port=emdawnwebgpu %*
--- Build.sh
+++ Build.sh
@@ -4,5 +4,5 @@ mkdir -p Build
 
 emcc -std=c++20 -g -gsource-map -Wall -Wextra -Werror -ferror-limit=0 \
     -Wsign-conversion -Wno-unused-parameter -Wno-missing-designated-field-initializers \
-    Source/BrinkSandbox.cpp --shell-file Source/Brink.html -o Build/index.html \
+    Source/BkSandbox.cpp --shell-file Source/BkSandbox.html -o Build/index.html \
     --use-port=emdawnwebgpu $@
