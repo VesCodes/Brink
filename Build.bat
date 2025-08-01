57bc20e41e01fd163f8ead2cd19159ee3d383254
VesCodes <1884844+VesCodes@users.noreply.github.com>
2025-08-01 23:56:40 +0100

    Build: Bootstrap build tool via build scripts

--- Build.bat
+++ Build.bat
@@ -2,8 +2,12 @@
 
 if not exist Build mkdir Build
 
-emcc -std=c++20 -g -gsource-map -Wall -Wextra -Werror -ferror-limit=0 ^
-    -Wsign-conversion -Wno-unused-parameter -Wno-missing-designated-field-initializers ^
-    -I"Source" -isystem"ThirdParty" ^
-    Source/Sandbox/Sandbox.cpp --shell-file Source/Sandbox/Sandbox.html -o Build/index.html ^
-    --use-port=emdawnwebgpu %*
+if not exist "Build/BkBuild.exe" (
+	clang++ -std=c++20 -g -Wall -Werror -I "Source" -I "ThirdParty" "Source/Build/Build.cpp" -o "Build/BkBuild.exe"
+)
+
+em++ -std=c++20 -g -gsource-map -Wall -Wextra -Werror -ferror-limit=0 ^
+	-Wsign-conversion -Wno-unused-parameter -Wno-missing-designated-field-initializers ^
+	-I"Source" -isystem"ThirdParty" ^
+	Source/Sandbox/Sandbox.cpp --shell-file Source/Sandbox/Sandbox.html -o Build/index.html ^
+	--use-port=emdawnwebgpu %*
--- Build.sh
+++ Build.sh
@@ -2,8 +2,12 @@
 
 mkdir -p Build
 
-emcc -std=c++20 -g -gsource-map -Wall -Wextra -Werror -ferror-limit=0 \
-    -Wsign-conversion -Wno-unused-parameter -Wno-missing-designated-field-initializers \
-    -I"Source" -isystem"ThirdParty" \
-    Source/Sandbox/Sandbox.cpp --shell-file Source/Sandbox/Sandbox.html -o Build/index.html \
-    --use-port=emdawnwebgpu $@
+if [ ! -f "Build/BkBuild" ]; then
+	clang++ -std=c++20 -g -Wall -Werror -I "Source" -I "ThirdParty" "Source/Build/Build.cpp" -o "Build/BkBuild"
+fi
+
+em++ -std=c++20 -g -gsource-map -Wall -Wextra -Werror -ferror-limit=0 \
+	-Wsign-conversion -Wno-unused-parameter -Wno-missing-designated-field-initializers \
+	-I"Source" -isystem"ThirdParty" \
+	Source/Sandbox/Sandbox.cpp --shell-file Source/Sandbox/Sandbox.html -o Build/index.html \
+	--use-port=emdawnwebgpu $@
